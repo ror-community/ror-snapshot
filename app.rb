06d9f21f374155c0a506f849812a0fe81a88506f
Esha Datta <edatta@crossref.org>
2018-12-14 11:33:59 -0500

    working on filter

--- app.rb
+++ app.rb
@@ -22,10 +22,63 @@ set :accepted_params, %w(query page filter query.name query.names)
 
 set :filter_types, %w(location type)
 
+set :accepted_param_values, %w(location types)
+
 def search_all(start = 0, size = settings.default_size)
   settings.client.search from: start, size: size
 end
 
+def simple_query(term)
+  query_string do
+    query term
+  end
+end
+
+def match_field(field, term)
+  match field.to_sym do
+    query term
+    operator "and"
+  end
+end
+
+def multi_field_match(fields, term)
+  multi_match do
+    query    term
+    operator 'and'
+    fields   fields
+  end
+end
+
+# filter=types:blah,country.country_code, country.country_name
+def gen_filtered_query(filter_query_array)
+  query_type = filter_query_array.size > 1 ? {} : []
+  if filter_query_array.count > 1
+    query_type = []
+    filter_query.each { |f|
+      field,term = f.split(":")
+      query_type << match_field(field,term)
+    }
+  else
+    query_type = {}
+    field,term = filter_query_array[0].split(":")
+    binding.pry
+    query_type = match_field(field,term)
+  end
+  filter do
+    query_type
+  end
+end
+
+def gen_bool_query(query_type,filter_type)
+  query_type = filter_query_array.size > 1 ? {} : []
+  bool do
+    must do
+      query_type
+    end
+    filter do
+    end
+  end
+end
 # meta program so that one can build query strings depending on parameter
 # query.name is only name
 #query.names looks at name, aliases, labels
@@ -33,28 +86,29 @@ end
 # query term: {query: {match: {name: {query:"Bath Spa University",operator:"and"}}}}
 # create query strings by parameter
 def generate_query(options = {})
+  filter = nil
+  qt = nil
+  if options["filter"]
+    filter = options["filter"].split(",")
+  end
   q = search {
     query do
       if options.key?("query")
-        query_string do
-          query options["query"]
-        end
+        qt = simple_query(options["query"])
       elsif options.key?("query.name")
-        match :name do
-          query options["query.name"]
-          operator "and"
-        end
+        qt = match_field("name",options["query.name"])
       elsif options.key?("query.names")
-        multi_match do
-          query    options["query.names"]
-          operator 'and'
-          fields   %w[ name aliases acronyms labels.label ]
-        end
+        fields = %w[ name aliases acronyms labels.label ]
+        qt = multi_field_match(fields, options["query.names"])
       end
+      gen_bool_query(qt,filter) if filter
     end
   }
   q.to_hash
 end
+
+
+
 
 def process (options = {})
   msg = nil
